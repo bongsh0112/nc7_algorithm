130adcb0d41797f5f0748f12382ad0f24692d4da
donsonioc2010 <whddnjs822@gmail.com>
2023-06-01 11:32:15 +0900

    add : 문자열 - 단어의 갯수 풀이 추가

--- Members/JongWon/Week1/BOJ_11654.java
+++ Members/JongWon/Week1/BOJ_11654.java
@@ -3,6 +3,9 @@ package Members.JongWon.Week1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 문자열 - 아스키코드
+ */
 public class BOJ_11654 {
   public static void main(String[] args) throws Exception {
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
