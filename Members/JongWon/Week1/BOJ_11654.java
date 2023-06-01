1b12b3982e58376edb29df291b86294db35dfb3c
donsonioc2010 <whddnjs822@gmail.com>
2023-06-01 11:17:15 +0900

    add : 문자열 - 아스키코드 문제 풀이 완료

---
+++ Members/JongWon/Week1/BOJ_11654.java
@@ -0,0 +1,13 @@
+package Members.JongWon.Week1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_11654 {
+  public static void main(String[] args) throws Exception {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      int num = br.readLine().charAt(0);
+      System.out.println(num);
+    }
+  }
+}
