e6f4ebb8498764cd3d83729852ddb8b4ae5e89b6
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-06-02 14:59:43 +0900

    :technologist: 11654번 아스키코드 풀이

---
+++ Members/SeHwan/Week1/BOJ_11654.java
@@ -0,0 +1,17 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = bf.readLine().charAt(0);
+
+        System.out.println(n);
+    }
+
+}
