1d7ce5ffa4c48e7123e04e694bab26e1eb31bdfb
bongsh0112 <bongsh0112@gmail.com>
2023-06-09 20:56:40 +0900

    :technologist: 시간 복잡도 문제 풀이

---
+++ Members/SeHwan/Week2/BOJ_24262.java
@@ -0,0 +1,17 @@
+package Week2;
+
+public class BOJ_24262 {
+
+  public static void main(String[] args) {
+
+//    MenOfPassion(A[], n) {
+//      i = ⌊n / 2⌋;
+//      return A[i]; # 코드1
+//    }
+
+    System.out.println(1);
+    System.out.println(0);
+
+  }
+
+}
