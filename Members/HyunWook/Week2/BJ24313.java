361003498d60b3632e8960de3f275d6799f8fd41
wooki37 <lite1324@naver.com>
2023-06-11 16:33:53 +0900

    점근접 표기:package:

--- Members/HyunWook/Week2/BJ24313.java
+++ Members/HyunWook/Week2/BJ24313.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int f1 = scanner.nextInt();
-    int f2 = scanner.nextInt();
-    int c = scanner.nextInt();
-    int n = scanner.nextInt();
+    Scanner sc = new Scanner(System.in);
+    int f1 = sc.nextInt(); // 사용자로부터 값을 입력받아 변수에 저장
+    int f2 = sc.nextInt();
+    int c = sc.nextInt();
+    int n = sc.nextInt();
 
     int a = f1 * n + f2;
     int b = c * n;
 
+    // a가 b보다 작거나 같고, f1이 c보다 작거나 같은 경우에는 1을 출력하고, 그렇지 않은 경우에는 0을 출력합니다.
     if (a <= b && f1 <= c) {
       System.out.println(1);
     } else {
