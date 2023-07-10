f4865dfc3d8d8683dc3469763bbaa2a42dd5d24f
werybalert <hooil0916@gmail.com>
2023-07-10 17:56:46 +0900

    수들의 합 4

--- Members/Holy/Week6/BOJ_20543.java
+++
@@ -1,4 +0,0 @@
-package Members.Holy.Week6;
-
-public class BOJ_20543 {
-}
--- Members/Holy/Week6/BOJ_3187.java
+++
@@ -1,7 +0,0 @@
-package Members.Holy.Week6;
-
-public class BOJ_3187 {
-    public static void main(String[] args) {
-
-    }
-}
