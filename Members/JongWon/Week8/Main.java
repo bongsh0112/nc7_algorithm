c20c1d96ebdf297dc39db9ac165b461eff848249
donsonioc2010 <whddnjs822@gmail.com>
2023-07-20 01:41:41 +0900

    add : 귀찮아 템플릿 그냥 올려놀래

---
+++ Members/JongWon/Week8/Main.java
@@ -0,0 +1,12 @@
+package Members.JongWon.Week8;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws Exception {
+
+  }
+}
