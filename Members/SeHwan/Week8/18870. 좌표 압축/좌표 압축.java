798833cfaeb332cad40579145443d640025af1ad
bongsh0112 <bongsh0112@gmail.com>
2023-07-21 11:25:14 +0900

    :technologist: 18870 좌표 압축 설명 추가

--- Members/SeHwan/Week8/18870. 좌표 압축/좌표 압축.java
+++ Members/SeHwan/Week8/18870. 좌표 압축/좌표 압축.java
@@ -13,17 +13,17 @@ public class Main {
     int N = Integer.parseInt(bufferedReader.readLine());
     String[] str = bufferedReader.readLine().split(" ");
     
-    TreeSet<Integer> dots = new TreeSet<>();
-    int[] dotsArr = new int[N];
+    TreeSet<Integer> dots = new TreeSet<>(); // 중복제거와 오름차순 정렬을 위한 TreeSet
+    int[] dotsArr = new int[N]; // 입력으로 들어온 좌표들을 넣는 int 배열
     for (int i = 0; i < N; i++) {
       int temp = Integer.parseInt(str[i]);
-      dots.add(temp);
-      dotsArr[i] = temp;
+      dots.add(temp); // TreeSet에 저장 
+      dotsArr[i] = temp; // dotsArr에 저장
     }
     
-    List<Integer> vertical = new ArrayList<>(dots);
+    List<Integer> vertical = new ArrayList<>(dots); // TreeSet -> ArrayList. TreeSet의 원소들을 수직선에 놓는다고 생각하면 된다
     for (int dot : dotsArr) {
-      int idx = Collections.binarySearch(vertical, dot);
+      int idx = Collections.binarySearch(vertical, dot); // 수직선에서 dotsArr의 원소가 몇 번째인지 알아낸다
       stringBuilder.append(idx + " ");
     }
     
