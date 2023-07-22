cd3cfad56c580a571263c5370d67d84cd57b7e91
bongsh0112 <bongsh0112@gmail.com>
2023-07-22 23:20:55 +0900

    :technologist: 1931 회의실 배정 풀이.. 했는데 메모리 초과 :fire:

--- Members/SeHwan/Week8/1931. 회의실 배정.java
+++ Members/SeHwan/Week8/1931. 회의실 배정.java
@@ -6,15 +6,15 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-
+  
   static int N;
-
+  
   static List<TimeStamp> timeList = new ArrayList<>();
   static List<Integer> startArr = new ArrayList<>(); // 회의 시작시간
   static List<Integer> endArr = new ArrayList<>(); // 회의 끝시간
-
+  
   public static void main(String[] args) throws IOException {
-
+    
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     N = Integer.parseInt(bufferedReader.readLine());
     int[] counts = new int[N]; // 시작시간마다 따져보는 가능한 회의의 개수
@@ -26,11 +26,11 @@ public class Main {
       endArr.add(end);
       timeList.add(new TimeStamp(start, end));
     }
-
+    
     int maxTime = Collections.max(endArr); // 끝 시간
     startArr.sort((a, b) -> a - b);
     timeList.sort((a, b) -> a.getStart() - b.getStart());
-
+    
     /**
      * for (TimeStamp t : timeList) {
      *   System.out.println(t);
@@ -47,7 +47,7 @@ public class Main {
      * timeStamp = TimeStamp{start=8, end=12}
      * timeStamp = TimeStamp{start=12, end=14}
      */
-
+    
     for (int i = 0; i < N; i++) {
       int start = startArr.get(i);
       int end = timeList.get(i).getEnd();
@@ -61,19 +61,15 @@ public class Main {
         end = timeList.get(startArr.indexOf(start)).getEnd();
         count++;
       }
-
+      
       counts[i] = count;
     }
 
-//    List<Integer> result = IntStream.of(counts).boxed().toList();
-    List<Integer> result = new ArrayList<>();
-    for (int i : counts) {
-      result.add(i);
-    }
+    List<Integer> result = IntStream.of(counts).boxed().toList();
     System.out.println(Collections.max(result));
-
+    
   }
-
+  
   static int getMinTime(int t) {
     List<Integer> tempStartArr = new ArrayList<>(startArr);
     List<Integer> list = new ArrayList<>();
@@ -83,27 +79,27 @@ public class Main {
         list.add(tempStartArr.get(i));
       }
     }
-
+    
     if (list.isEmpty()) {
       return -1;
     } else {
       return Collections.min(list);
     }
   }
-
+  
   static class TimeStamp {
     private int start;
     private int end;
-
+    
     public TimeStamp(int start, int end) {
       this.start = start;
       this.end = end;
     }
-
+    
     public int getStart() {
       return start;
     }
-
+    
     public int getEnd() {
       return end;
     }
