0cd82d321fdbb73aff35a619eed7e19cacf228b6
donsonioc2010 <whddnjs822@gmail.com>
2023-07-26 09:21:03 +0900

    add : Template

---
+++ Members/JongWon/Week9/Main.java
@@ -0,0 +1,14 @@
+package Members.JongWon.Week9;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  // 건물은 1층부터
+  public static void main(String[] args) throws Exception {
+
+  }
+}
