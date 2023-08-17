c72f0ef8e88e4003e2e2d75e2f53646ad9404ff4
donsonioc2010 <whddnjs822@gmail.com>
2023-08-17 20:17:37 +0900

    Week 11 Init

---
+++ Members/JongWon/Week11/Main.java
@@ -0,0 +1,14 @@
+package Members.JongWon.Week11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws Exception {
+
+  }
+
+}
